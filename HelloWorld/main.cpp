9ce64a945494301bb61b3ae3c46bbb2e0329fa9a
The-Bearded-Vegan <63019033+The-Bearded-Vegan@users.noreply.github.com>
2020-04-01 22:02:52 +0100

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Richard Cheesman on 01/04/2020.
+//  Copyright © 2020 Richard Cheesman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
